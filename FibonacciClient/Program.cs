6630ce1323c0a0d8c96d2c49dacb8b8c811ec6be
Szymurski <JSzymurski@tainstruments.com>
2021-03-26 16:24:00 -0400

    Initial

---
+++ FibonacciClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+using FibonacciGRPC;
+
+namespace FibonacciClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Channel channel = new Channel(@"localhost:50051", ChannelCredentials.Insecure);
+
+            bool useStream = true;
+            var client = new Fibonacci.FibonacciClient(channel);
+
+            if (useStream)
+            {
+                Console.Write("Enter how many Digits of Fibonacci you would like to receive: ");
+                String number = Console.ReadLine();
+                int seqNumber = Int32.Parse(number);
+                using (var stream = client.StreamFibonacci(new FibonacciRequest { Number = seqNumber }))
+                {
+                    while (await stream.ResponseStream.MoveNext())
+                    {
+                        Console.WriteLine(stream.ResponseStream.Current.Result);
+                    }
+                }
+                Console.ReadLine();
+            }
+            else
+            {
+                while (true)
+                {
+                    Console.Write("Enter Sequence Number: ");
+                    String number = Console.ReadLine();
+                    int seqNumber = Int32.Parse(number);
+                    var reply = client.FindFibonacci(new FibonacciRequest { Number = seqNumber });
+                    Console.WriteLine(String.Format("Fibonacci gRPC Service Response: {0}", reply.Result));
+                }
+            } 
+        }
+    }
+}
---
+++ FibonacciGRPC/Services/FibonacciService.cs
@@ -0,0 +1,63 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace FibonacciGRPC.Services
+{
+    public class FibonacciService : Fibonacci.FibonacciBase
+    {
+
+        private static Dictionary<int, BigInteger> _dictionary = new Dictionary<int, BigInteger>();
+
+
+        public override Task<FibonacciResult> FindFibonacci(FibonacciRequest request, ServerCallContext context)
+        {
+            int seqNumber = request.Number;
+
+            return Task.FromResult(new FibonacciResult
+            {
+                Result = FibAlgorithm(seqNumber).ToString()
+            });
+        }
+
+        public override async Task StreamFibonacci(FibonacciRequest request, IServerStreamWriter<FibonacciResult> responseStream, ServerCallContext context)
+        {
+            int fibCount = request.Number;
+
+            int index = 0;
+            while (index < fibCount)
+            {
+                var result = new FibonacciResult { Result = QuickFibAlgorithm(index).ToString() };
+                await responseStream.WriteAsync(result);
+                index++;
+            }
+        }
+
+
+        private static int FibAlgorithm(int n)
+        {
+            if (n == 0 || n == 1)
+            {                
+                return n;
+            }
+
+            return FibAlgorithm(n - 1) + FibAlgorithm(n - 2);
+        }
+
+        private static BigInteger QuickFibAlgorithm(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                _dictionary[n] = n;
+                return n;
+            }
+
+            _dictionary[n] = _dictionary[n - 1] + _dictionary[n - 2];
+
+            return _dictionary[n];
+        }
+    }
+}
